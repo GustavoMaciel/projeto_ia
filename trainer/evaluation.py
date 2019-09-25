65856a48f6fb33dc192f419b08254fb5a2109ee2
Gustavo Maciel Nunes <gm.nunes92@gmail.com>
2019-09-25 11:24:40 -0300

    Trying binary

--- trainer/evaluation.py
+++ trainer/evaluation.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from keras.models import model_from_json
 from keras.preprocessing.sequence import pad_sequences
+import numpy as np
 import settings
 import trainer_lib as lib
 
@@ -8,31 +9,58 @@ import trainer_lib as lib
 def evaluate_code(code, _model, tok_voc):
     tokens = lib.load_tokens(code)
     predict = tok_voc.texts_to_sequences(tokens)
+
+    tokens = [tok[0] for tok in predict if len(tok) > 0]
+    predict = pad_sequences([tokens], 79)
+
     return _model.predict(predict)
 
 
-with open(settings.model_file_location, encoding='latin1') as file:
+with open(settings.model_file_location) as file:
     model_json = file.read()
 
 
-model = model_from_json(settings.model_file_location)
+model = model_from_json(model_json)
 model.load_weights(settings.model_weights_file_location)
 
 tokenized_vocabulary = lib.load_tokenized_vocabulary(settings.tokenized_vocabulary_location)
 
 
 python = """
-def python(re):
-    return re
+class DeclarativeFieldsMetaclass(MediaDefiningClass):
+    def __new__(mcs, name, bases, attrs):
+        # Collect fields from current class.
+        current_fields = []
+        for key, value in list(attrs.items()):
+            if isinstance(value, Field):
+                current_fields.append((key, value))
+                attrs.pop(key)
+        attrs['declared_fields'] = dict(current_fields)
+
+        new_class = super(DeclarativeFieldsMetaclass, mcs).__new__(mcs, name, bases, attrs)
+
+        # Walk through the MRO.
+        declared_fields = {}
+        for base in reversed(new_class.__mro__):
+            # Collect fields from base class.
+            if hasattr(base, 'declared_fields'):
+                declared_fields.update(base.declared_fields)
 """
 
 java = """
+import javax.swing.*
 public class Main(){
     public static void main(String[] args){
+        int i = 0 + 2
+        System.out.println(i);
         System.out.println("Hello world!");
     }
 }
 """
 
-print(evaluate_code(java, model, tokenized_vocabulary))
 
+i = evaluate_code(java, model, tokenized_vocabulary)
+j = evaluate_code(python, model, tokenized_vocabulary)
+#i = np.argmax(i)
+
+print(i, j)
--- trainer/trainer.py
+++ trainer/trainer.py
@@ -16,20 +16,23 @@ tokenized_vocabulary = lib.tokenized_vocabulary_builder(vocabulary)
 test_voc, test_labels = lib.vocabulary_label_builder(settings.testing_data_dir, settings.minimal_amount_of_repetitions)
 
 
-lib.save_tokenized_vocabulary(settings.tokenized_vocabulary_location, tokenized_vocabulary)
+# lib.save_tokenized_vocabulary(settings.tokenized_vocabulary_location, tokenized_vocabulary)
 
-# model_max_features = 10000
+
 # model = lib.model_builder(tokenized_vocabulary, vocabulary, labels, tokenized_test_voc, test_voc, test_labels,
                        # model_max_features)
+
 
 
 tokens = tokenized_vocabulary.texts_to_sequences(vocabulary)
 input_dim = lib.get_input_dim(tokens)
 
+
 X = pad_sequences(tokens, input_dim)
 
-labels = keras.utils.to_categorical(labels, num_classes=2)
+#labels = keras.utils.to_categorical(labels, num_classes=2)
 Y = np.asarray(labels)
+
 
 #print(f"{len(tokens)}  {len(tokens)}")
 #print(tokens[40])
@@ -40,7 +43,7 @@ test_tokens = tokenized_vocabulary.texts_to_sequences(test_voc)
 
 X_test = pad_sequences(test_tokens, input_dim)
 
-test_labels = keras.utils.to_categorical(test_labels, num_classes=2)
+#test_labels = keras.utils.to_categorical(test_labels, num_classes=2)
 Y_test = np.asarray(test_labels)
 #print(f"{len(test_tokens)} {len(test_labels)}")
 #print(test_tokens[0])
@@ -57,28 +60,35 @@ model = Sequential()
 # model.add(keras.layers.GlobalAveragePooling1D())
 
 # Dense é uma camada, primeiro parametro é numero de neuronios, input_dim é a qtd de entradas dos neuronios
-model.add(Dense(12, activation='relu', input_dim=input_dim))
-model.add(Dense(12, activation='relu'))
+model.add(Dense(32, activation='relu', input_dim=input_dim))
+
 # Ultima camada é o número de classificações que queremos, softmax é pra considerar cada classificação de forma separada
-model.add(Dense(2, activation='softmax'))
+model.add(Dense(1, activation='sigmoid'))
 
 # Definir que será usado a descida gradiente
 sgd = SGD(lr=0.01)
 
 # Optimizador é o descida gradiente, varias classes com softmax a medição de erro melhor é categorical_crossentropy
 # metrics é medir a acurácia, taxa de acerto, do model
-model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=sgd, loss='binary_crossentropy', metrics=['accuracy'])
 
 # Epochs são o número de repetições pra cada exemplo
 # Batch é o número de iterações que precisam ser feitos antes de atualizar o gradiente
-model.fit(X, Y, epochs=500, batch_size=5)
+model.fit(X, Y, epochs=100, batch_size=5)
 score, acc = model.evaluate(X_test, Y_test, verbose=2, batch_size=2)
 
-model.save(settings.model_file_location)
-model.save_weights(settings.model_weights_file_location)
 
 print(f"\nInput dim: {input_dim}")
 print(model.metrics_names)
 print('Loss: %f' % score)
 print('Accuracy: %f' % acc)
+
+
+
+# model.save(settings.model_file_location)
+save = input("Save model? y/n ")
+if save.lower() == "y":
+    with open(settings.model_file_location, 'w') as file:
+        file.write(model.to_json())
+    model.save_weights(settings.model_weights_file_location)
 
